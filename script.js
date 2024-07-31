efea9c3b4762d1080bf0e6dd909612963721036b
aakanshapatil29 <162541345+aakanshapatil29@users.noreply.github.com>
2024-07-31 18:11:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addTask(){
+
+    if(inputBox.value == ' ')
+    {
+        alert("please write a task ");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML= "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = " ";
+    saveData();
+}
+
+listcontainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData()
+{
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showList()
+{
+    listcontainer.innerHTML = localStorage.getItem("data")
+}
+showList();
